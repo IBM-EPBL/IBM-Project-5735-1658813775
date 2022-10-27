efeb5f22a2d8bc8388cd6004c78e8d2f199f6d49
Girisurya <115545087+Girisurya@users.noreply.github.com>
2022-10-27 14:12:26 +0530

    Delete IBM-Project-5735-1658813775-main directory

--- IBM-Project-5735-1658813775-main/ASSIGNMENT/TEAM LEAD/ASSIGNMENT 2/sqlite_db_setup.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('signin_database.db')
-print("Opened database successfully")
-
-#conn.execute('CREATE TABLE signup (Name TEXT,email  TEXT,password  TEXT,retype password  TEXT)')
-conn.execute('CREATE TABLE signin email  TEXT,password  TEXT)')
-print("Table created successfully")
-conn.close()
